8413d90e4f7b2bf51378ba1031f619500481275f
github username <github email address>
2017-11-09 09:37:38 -0500

    added component folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import UserContainer from './components/User/UserContainer.js'
+import GameContainer from './components/Game/GameContainer.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>BACONNNNNN</h1>
+        <UserContainer />
+        <GameContainer />
       </div>
     );
   }
---
+++ src/components/Game/GameContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class GameContainer extends React.Component {
+  state = {
+
+  }
+
+
+  render() {
+    return (
+      <p>test</p>
+    )
+  }
+}
