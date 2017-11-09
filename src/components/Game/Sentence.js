2ade0896c8f043afd08ae26201ff00c27a3c8e6a
github username <github email address>
2017-11-09 13:38:37 -0500

    added sentence

--- src/components/Game/Sentence.js
+++ src/components/Game/Sentence.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react'
 
 export default class Sentence extends Component {
 	state = {
-
+		randSentence: "I AM IN A GLASS CASE OF EMOTION"
 	}
+
+
 
 	render(){
 		return(
 			<div className="sentence">
-				<span className="h1-style">Sentence goes here.</span>
+				<span className="h1-style">{this.state.randSentence}</span>
 			</div>
 		)
 	}
