9c8dd203b219995f22ae08ca36dd7a1bebf44cb8
github username <github email address>
2017-11-09 13:03:48 -0500

    added onChange to text input

--- src/components/Game/TextField.js
+++ src/components/Game/TextField.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react'
 
 export default class TextField extends Component {
 	state = {
+		typedText: ""
+	}
 
+	handleChange = (event) => {
+		this.setState({
+			typedText: event.target.value
+		})
 	}
 
 	render(){
 		return(
 			<div className="text-field">
-				<input type="text" className="input" placeholder="SEND ME A MESSAGE" id="type-here"/>
+				<input type="text" className="input" placeholder="SEND ME A MESSAGE" id="type-here" onChange={this.handleChange}/>
 			</div>
 		)
 	}
