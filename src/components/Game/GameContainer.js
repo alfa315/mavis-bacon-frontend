14c9ecea47fe3ac7bacecd408d0ab39e00bd6a50
github username <github email address>
2017-11-10 13:56:15 -0500

    fixed input logic except for 0 case

--- src/components/Game/GameContainer.js
+++ src/components/Game/GameContainer.js
@@ -6,47 +6,56 @@ import RaceTrack from './RaceTrack.js'
 export default class GameContainer extends Component {
 	state = {
 		randSentence: "I AM IN A GLASS CASE OF EMOTION",
+		currentText: "",
     currentCharCode: 0,
 		currentCharIndex: 0,
 		lastPressCorrect: true,
 		textLength: 1
 	}
 
-	handleKeyPress = (event) => {
-		this.setState({
-			currentCharCode: event.charCode
-		})
+	handleKeyDown = (event) => {
+		console.log(event.keyCode);
+
+		if(event.keyCode === 8 || event.keyCode === 46) {
+			this.setState({
+				currentCharCode: 0,
+				textLength: this.state.textLength - 1,
+				currentCharIndex: this.state.currentCharIndex - 1
+			})
+		} else if(event.keyCode !== this.state.randSentence.charCodeAt(this.state.currentCharIndex)) {
+			event.preventDefault()
+		} else {
+			this.setState({
+				currentCharCode: event.keyCode
+			})
+		}
 	}
 
 	handleChange = (event) => {
-		let textLength = event.target.value.length
-		this.setState({
-			currentCharIndex: textLength
-		}, this.compareCurrentChar())
-	}
-
-	compareCurrentChar = () => {
-		let { currentCharCode } = this.state
-		let { randSentence } = this.state
-		let { currentCharIndex } = this.state
-		if(currentCharCode === randSentence.charCodeAt(currentCharIndex)){
+		if(this.state.currentCharCode === this.state.randSentence.charCodeAt(this.state.currentCharIndex)){
 			this.setState({
-				textLength: this.state.textLength + 1,
+				currentCharIndex: event.target.value.length,
+				currentText: event.target.value,
+				textLength: event.target.value.length + 1,
 				lastPressCorrect: true
 			})
 		} else {
 			this.setState({
-				lastPressCorrect: false
+				lastPressCorrect: false,
 			})
 		}
 	}
 
 	render(){
+		console.log("Text:", this.state.currentText)
+		console.log("Text Length:", this.state.textLength)
+		console.log("Index:", this.state.currentCharIndex)
+		console.log("Char Code:", this.state.currentCharCode)
 		return(
 			<div className="game-container">
         <RaceTrack />
-				<Sentence textOutput={this.state.randSentence}/>
-				<TextField handleKeyPress={this.handleKeyPress} handleChange={this.handleChange} maxLen={this.state.textLength}/>
+				<Sentence textOutput={this.state.randSentence} />
+				<TextField handleKeyDown={this.handleKeyDown} handleChange={this.handleChange} maxLen={this.state.textLength}/>
 			</div>
 		)
 	}
--- src/components/Game/TextField.js
+++ src/components/Game/TextField.js
@@ -4,12 +4,12 @@ export default class TextField extends Component {
 	render(){
 		return(
 			<div className="text-field">
-				<input 
-					type="text" 
-					className="input" 
-					placeholder="SEND ME A MESSAGE" 
-					id="type-here" 
-					onKeyPress={this.props.handleKeyPress}
+				<input
+					type="text"
+					className="input"
+					placeholder="SEND ME A MESSAGE"
+					id="type-here"
+					onKeyDown={this.props.handleKeyDown}
 					onChange={this.props.handleChange}
 					maxLength={this.props.maxLen}
 				/>
