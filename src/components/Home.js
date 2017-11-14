1afe9789ee6ddab844a5f0d1b0dfda2e27ede4fc
jonopens <jonopenshaw@gmail.com>
2017-11-14 16:17:19 -0500

    added some style n stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ const Home = (props) => {
 	return (
 		<div>
 			<h1>Welcome to Marvis Bacon, the Typing Race Game</h1>
-			<p>I'm using this to create some space.</p>
+			<div className="ui segment">
+				<p className="larger-text">Be ready to type as soon as the next screen loads!</p>
+				<p className="larger-text">You're racing Marvis Bacon (and all the other players of all time) for a coveted seat on the <strong>Top 10</strong></p>
+			</div>
 			<Leaderboard games={props.games} />
 		</div>
 	)
