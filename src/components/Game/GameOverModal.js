346eeed7102d7204e0fdcfd433fb03f258505021
github username <github email address>
2017-11-14 09:29:09 -0500

    fixed game winner and updated modals

--- src/components/Game/GameOverModal.js
+++ src/components/Game/GameOverModal.js
@@ -3,20 +3,36 @@ import React from 'react'
 export default class GameOverModal extends React.Component {
   render() {
     if (this.props.gameWinner === true) {
+      this.props.fetchGames()
       return(
         <div className='modal-win' style={{display: this.props.gameOverModal}}>
-        <div className='modal-content-win'>
-        <span className='close-win'> </span>
-        <p>YOU WON, YOU BEAT MARVIS! YOUR WORDS PER MINUTE WAS: {this.props.wpm()}</p>
-        </div>
+          <div className='modal-content-win'>
+            <div className='modal-header-win'>
+              <span className='close-win'> </span>
+              <h2> CONGRATULATIONS</h2>
+            </div>
+            <div className='modal-body-win'>
+              <p>YOU WON, YOU BEAT MARVIS! YOUR WORDS PER MINUTE WAS: {this.props.wpm()}</p>
+            </div>
+            <div className='modal-footer-win'>
+            </div>
+          </div>
         </div>
       )
     } else if(this.props.gameWinner === false && this.props.gameOverModal === "block") {
+        this.props.fetchGames()
         return(
           <div className='modal-lose' style={{display: this.props.gameOverModal}}>
             <div className='modal-content-lose'>
-              <span className='close-lose'> </span>
-              <p>YOU LOSE, MARVIS IS THE BEST! YOUR WORDS PER MINUTE WAS: {this.props.wpm()}</p>
+              <div className='modal-header-lose'>
+                <span className='close-lose'> </span>
+                <h2> BOOOOOOOOOOO </h2>
+              </div>
+              <div className='modal-body-lose'>
+                <p>YOU LOSE, MARVIS IS THE BEST! YOUR WORDS PER MINUTE WAS: {this.props.wpm()}</p>
+              </div>
+              <div className='modal-footer-lose'>
+              </div>
             </div>
           </div>
         )
