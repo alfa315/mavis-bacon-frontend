244f251c791567922f7ac37a1d5f1deafda340d9
jonopens <jonopenshaw@gmail.com>
2017-11-15 13:27:47 -0500

    now with more bacon

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ const Home = (props) => {
 			<h1>Welcome to Marvis Bacon, the Typing Race Game</h1>
 			<div className="ui segment">
 				<p className="larger-text">Be ready to type as soon as the next screen loads!</p>
+				<p><img src="http://www.pngmart.com/files/3/Bacon-PNG-Image.png" alt="bacon" height="300px"/></p>
 				<p className="larger-text">You're racing Marvis Bacon (and all the other players of all time) for a coveted seat on the <strong>Top 10</strong></p>
 			</div>
 			<Leaderboard games={props.games} />
